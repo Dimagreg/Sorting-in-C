5e90a08375f16b7a3e57e9d1c73bd93cd68faa9c
DimagregUPT <dmitrii.grigori@student.upt.ro>
2023-07-20 13:57:35 +0300

    change from int to long add bubble sort

--- lib/my_sort.h
+++ lib/my_sort.h
@@ -1,7 +1,13 @@
 #ifndef __MY_SORT_H
 #define __MY_SORT_H
 
+void
+my_shuffle_array (long *file_numbers_array, long lines_count);
+
 int
-my_insertion_sort (unsigned int *arr, int size);
+my_insertion_sort (long *arr, long size);
+
+int
+my_bubble_sort (long *arr, long size);
 
 #endif //__MY_SORT_H
