fb4c811fd2edc6c8cc1d675e5995514df52bee0d
Dimagreg <dima@grigori.md>
2023-11-12 15:28:17 +0200

    move auxiliary functions to my_sort_aux.c

--- my_sort.c
+++ my_sort.c
@@ -4,16 +4,9 @@
 #include <string.h>
 
 #include "lib/my_print.h"
+#include "lib/my_sort_aux.h"
 
 const long RUN = 32;
-
-void 
-swap (long *xp, long *yp)
-{
-    long temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
 
 /* Randomly shuffle elements of array. */
 void
@@ -39,9 +32,7 @@ my_shuffle_array (long *file_numbers_array, long lines_count)
     }
 }
 
-
 /*Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
-
 Time Complexity: O(N^2)
 Average time to sort 100,000 numbers: 14.50s 
 */
@@ -68,7 +59,6 @@ insertionSort (long *arr, long size)
 }
 
 /* In this algorithm, traverse from left and compare adjacent elements and the higher one is placed at right side. In this way, the largest element is moved to the rightmost end at first. This process is then continued to find the second largest and place it and so on until the data is sorted.
-
 Time Complexity: O(N^2)
 Average time to sort 100,000 numbers: 62.55s 
 */
@@ -101,7 +91,6 @@ bubbleSort (long *arr, long size)
 }
 
 /*Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list. 
-
 Time Complexity: O(N^2)
 Average time to sort 100,000 numbers: 55.88s 
 */
@@ -128,61 +117,9 @@ selectionSort (long *arr, long n)
 }
 
 /*Merge sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.
-
 Time Complexity: O(N log(N))
 Average time to sort 100,000 numbers: ?
 */
-void 
-merge (long arr[], long l, long m, long r)
-{
-    long i, j, k;
-    long n1 = m - l + 1;
-    long n2 = r - m;
- 
-    // Create temp arrays
-    long L[n1], R[n2];
- 
-    // Copy data to temp arrays L[] and R[]
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
- 
-    // Merge the temp arrays back into arr[l..r
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
- 
-    // Copy the remaining elements of L[],
-    // if there are any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
- 
-    // Copy the remaining elements of R[],
-    // if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
- 
-// l is for left index and r is right index of the
-// sub-array of arr to be sorted
 int 
 mergeSort (long arr[], long l, long r)
 {
@@ -200,38 +137,11 @@ mergeSort (long arr[], long l, long r)
 }
 
 /*QuickSort is a sorting algorithm based on the Divide and Conquer algorithm that picks an element as a pivot and partitions the given array around the picked pivot by placing the pivot in its correct position in the sorted array.
-
 Time Complexity: O(N log(N))
 Average time to sort 100,000 numbers: ?
-*/
-long 
-partition (long arr[],long low,long high)
-{
-  //choose the pivot
-   
-  long pivot=arr[high];
-  //Index of smaller element and Indicate
-  //the right position of pivot found so far
-  long i=(low-1);
-   
-  for(long j=low;j<=high;j++)
-  {
-    //If current element is smaller than the pivot
-    if(arr[j]<pivot)
-    {
-      //Increment index of smaller element
-      i++;
-      swap(&arr[i],&arr[j]);
-    }
-  }
-  swap(&arr[i+1],&arr[high]);
-  return (i+1);
-}
- 
-// The Quicksort function Implement
-            
+*/        
 int 
-quickSort (long arr[],long low,long high) //TODO: test
+quickSort (long arr[],long low,long high)
 {
   // when low is less than high
   if(low<high)
@@ -248,46 +158,6 @@ quickSort (long arr[],long low,long high) //TODO: test
   }
 
   return 1;
-}
-
-
-void 
-heapify (long arr[], long N, long i) //TODO: on make gives warnings due to swap function
-{
-    // Find largest among root,
-    // left child and right child
- 
-    // Initialize largest as root
-    long largest = i;
- 
-    // left = 2*i + 1
-    long left = 2 * i + 1;
- 
-    // right = 2*i + 2
-    long right = 2 * i + 2;
- 
-    // If left child is larger than root
-    if (left < N && arr[left] > arr[largest])
- 
-        largest = left;
- 
-    // If right child is larger than largest
-    // so far
-    if (right < N && arr[right] > arr[largest])
- 
-        largest = right;
- 
-    // Swap and continue heapifying
-    // if root is not largest
-    // If largest is not root
-    if (largest != i) {
- 
-        swap(&arr[i], &arr[largest]);
- 
-        // Recursively heapify the affected
-        // sub-tree
-        heapify(arr, N, largest);
-    }
 }
  
 /*Heap sort is a comparison-based sorting technique based on Binary Heap data structure. It is similar to the selection sort where we first find the minimum element and place the minimum element at the beginning. Repeat the same process for the remaining elements.
@@ -316,18 +186,6 @@ heapSort(long arr[], long N)
 
     return 1;
 }
-
-long 
-getMax (long a[], long n) {  
-   long max = a[0];  
-   
-   for (long i = 1; i < n; i++)
-   {  
-    if (a[i] > max)  
-        max = a[i];  
-   }  
-   return max; //maximum element from the array  
-}  
   
 /*Counting Sort is a non-comparison-based sorting algorithm that works well when there is limited range of input values. It is particularly efficient when the range of input values is small compared to the number of elements to be sorted. The basic idea behind Counting Sort is to count the frequency of each distinct element in the input array and use that information to place the elements in their correct sorted positions.
 Time Complexity: O(N+M), where N and M are the size of inputArray[] and countArray[] respectively.
@@ -368,31 +226,6 @@ countSort (long a[], long n)
     }  
 
     return 1;
-}  
-
-void 
-countingSort (long a[], long n, long place) // counting sort for radix  
-{  
-    long output[n + 1];  
-    long count[10] = {0};    
-    
-    // Calculate count of elements  
-    for (long i = 0; i < n; i++)  
-        count[(a[i] / place) % 10]++;  
-        
-    // Calculate cumulative frequency  
-    for (long i = 1; i < 10; i++)  
-        count[i] += count[i - 1];  
-    
-    // Place the elements in sorted order  
-    for (long i = n - 1; i >= 0; i--) 
-    {  
-        output[count[(a[i] / place) % 10] - 1] = a[i];  
-        count[(a[i] / place) % 10]--;  
-    }  
-    
-    for (long i = 0; i < n; i++)  
-        a[i] = output[i];  
 }  
   
 /*Radix Sort is a linear sorting algorithm that sorts elements by processing them digit by digit. It is an efficient sorting algorithm for integers or strings with fixed-size keys. 
@@ -515,23 +348,6 @@ shellSort (long arr[], long n)
     return 1; 
 }
 
-// This function sorts array from left
-// index to to right index which is
-// of size atmost RUN
-void 
-insertionsort (long arr[], long left, long right)
-{
-    for (long i = left + 1; i <= right; i++) {
-        int temp = arr[i];
-        int j = i - 1;
-        while (j >= left && arr[j] > temp) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = temp;
-    }
-}
-
 /*Tim Sort is a hybrid sorting algorithm derived from merge sort and insertion sort. It was designed to perform well on many kinds of real-world data. Tim Sort is the default sorting algorithm used by Python’s sorted() and list.sort() functions.
 Time Complexity: O(n*log(n))
 Average time to sort 100,000 numbers: ?
@@ -574,18 +390,6 @@ timSort (long arr[], long n)
     }
 
     return 1;
-}
-
-// To find gap between elements
-int 
-getNextGap (int gap)
-{
-    // Shrink gap by Shrink factor
-    gap = (gap * 10) / 13;
- 
-    if (gap < 1)
-        return 1;
-    return gap;
 }
  
 /*Comb Sort is mainly an improvement over Bubble Sort. Bubble sort always compares adjacent values. So all inversions are removed one by one. Comb Sort improves on Bubble Sort by using a gap of the size of more than 1. The gap starts with a large value and shrinks by a factor of 1.3 in every iteration until it reaches the value 1. Thus Comb Sort removes more than one inversion count with one swap and performs better than Bubble Sort.
@@ -786,36 +590,8 @@ cocktailSort (long a[], long n)
 
     return 1;
 }
-
-/*The parameter dir indicates the sorting direction, ASCENDING
-   or DESCENDING; if (a[i] > a[j]) agrees with the direction,
-   then a[i] and a[j] are interchanged.*/
-void 
-compAndSwap (long a[], long i, long j, long dir)
-{
-    if (dir==(a[i]>a[j]))
-        swap(&a[i],&a[j]);
-}
- 
-/*It recursively sorts a bitonic sequence in ascending order,
-  if dir = 1, and in descending order otherwise (means dir=0).
-  The sequence to be sorted starts at index position low,
-  the parameter cnt is the number of elements to be sorted.*/
-void 
-bitonicMerge (long a[], long low, long cnt, long dir)
-{
-    if (cnt>1)
-    {
-        long k = cnt/2;
-        for (long i=low; i<low+k; i++)
-            compAndSwap(a, i, i+k, dir);
-        bitonicMerge(a, low, k, dir);
-        bitonicMerge(a, low+k, k, dir);
-    }
-}
  
 /*Bitonic Sort is a classic parallel algorithm for sorting. 
-
 The number of comparisons done by Bitonic sort is more than popular sorting algorithms like Merge Sort [ does O(log N) comparisons], but Bitonic sort is better for parallel implementation because we always compare elements in a predefined sequence and the sequence of comparison doesn’t depend on data. Therefore it is suitable for implementation in hardware and parallel processor array.
 Bitonic Sort can only be done if the number of elements to sort is 2^n. The procedure of bitonic sequence fails if the number of elements is not in the aforementioned quantity precisely.
 Time complexity: O(log n)
@@ -842,33 +618,6 @@ bitonicSort (long a[],long low, long cnt, long dir)
     }
 
     return 0;
-}
-
-/* Reverses arr[0..i] */
-void 
-flip (long arr[], long i)
-{
-    long temp, start = 0;
-    while (start < i) {
-        temp = arr[start];
-        arr[start] = arr[i];
-        arr[i] = temp;
-        start++;
-        i--;
-    }
-}
- 
-// Returns index of the
-// maximum element in
-// arr[0..n-1]
-long 
-findMax (long arr[], long n)
-{
-    long mi, i;
-    for (mi = 0, i = 0; i < n; ++i)
-        if (arr[i] > arr[mi])
-            mi = i;
-    return mi;
 }
  
 /*Pancake sorting is the mathematical problem of sorting a disordered stack of pancakes in order of size when a spatula can be inserted at any point in the stack and used to flip all pancakes above it. A pancake number is the minimum number of flips required for a given number of pancakes.
@@ -909,24 +658,6 @@ pancakeSort (long arr[], long n)
     }
 
     return 1;
-}
-
-// To check if array is sorted or not
-int 
-isSorted (long a[], long n)
-{
-    while (--n > 0)
-        if (a[n] < a[n - 1])
-            return 0;
-    return 1;
-}
- 
-// To generate permutation of the array
-void 
-shuffle (long a[], long n)
-{
-    for (long i = 0; i < n; i++)
-        swap(&a[i], &a[rand() % n]);
 }
  
 /*BogoSort also known as permutation sort, stupid sort, slow sort, shotgun sort or monkey sort is a particularly ineffective algorithm one person can ever imagine. It is based on generate and test paradigm. The algorithm successively generates permutations of its input until it finds one that is sorted.(Wiki) For example, if bogosort is used to sort a deck of cards, it would consist of checking if the deck were in order, and if it were not, one would throw the deck into the air, pick the cards up at random, and repeat the process until the deck is sorted.
